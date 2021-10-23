6976241f29c69d3bc1c749594c001cf167d2f471
alfar <alfredo.faria@maersk.com>
2021-10-23 23:22:10 +0200

    Initial commit

---
+++ src/main/java/com/example/hr/department/service/HrDepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr.department.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrDepartmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrDepartmentServiceApplication.class, args);
+    }
+
+}
